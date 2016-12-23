1671ec0d3e0d0b926247e2aee935a99a78f85758
Zohar Arad <zohar@zohararad.com>
2016-12-23 18:39:41 +0200

    fix unassigned db Conn variable in db/conn

--- db/conn.go
+++ db/conn.go
@@ -2,13 +2,15 @@ package db
 
 import (
   "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
   "fmt"
 )
 
 var Conn *gorm.DB
 
-func Connect(host string, database string, user string, pass string) (*gorm.DB, error) {
+func Connect(host string, database string, user string, pass string) (db *gorm.DB, err error) {
   dbConnString := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", host, user, database, pass)
-  Conn, err := gorm.Open("postgres", dbConnString)
-  return Conn, err
+  db, err = gorm.Open("postgres", dbConnString)
+  Conn = db
+  return
 }
